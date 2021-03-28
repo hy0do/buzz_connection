ccb4b6c40d17071f48533ff567e0ac1fbfda7c4a
hy0do <tatsuya.hyohdoh@gmail.com>
2021-03-28 23:42:45 +0900

    add wss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = "wss://#{ENV['HOST_NAME']}/cable"
   config.action_cable.allowed_request_origins = [ ENV['HOST_URL'] ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
